dd075b0cda99b31b7c1cd0aa6036477bd6b46a25
SAMAEL <SAMAEL@SAMAEL-PC>
2017-04-11 17:48:28 -0500

    cerrar los frames.

--- src/vista/paginaInicial.java
+++ src/vista/paginaInicial.java
@@ -39,8 +39,10 @@ public class paginaInicial extends javax.swing.JFrame {
         panelFondoPaginaInicial1 = new vista.PanelFondoPaginaInicial();
         jButton2 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButton2.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
@@ -62,25 +64,36 @@ public class paginaInicial extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Documentos/cerrarIcon.png"))); // NOI18N
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout panelFondoPaginaInicial1Layout = new javax.swing.GroupLayout(panelFondoPaginaInicial1);
         panelFondoPaginaInicial1.setLayout(panelFondoPaginaInicial1Layout);
         panelFondoPaginaInicial1Layout.setHorizontalGroup(
             panelFondoPaginaInicial1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFondoPaginaInicial1Layout.createSequentialGroup()
+            .addGroup(panelFondoPaginaInicial1Layout.createSequentialGroup()
                 .addContainerGap(105, Short.MAX_VALUE)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(51, 51, 51)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(62, 62, 62))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFondoPaginaInicial1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         panelFondoPaginaInicial1Layout.setVerticalGroup(
             panelFondoPaginaInicial1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelFondoPaginaInicial1Layout.createSequentialGroup()
-                .addGap(103, 103, 103)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69)
                 .addGroup(panelFondoPaginaInicial1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(126, Short.MAX_VALUE))
+                .addContainerGap(140, Short.MAX_VALUE))
         );
 
         jButton1.getAccessibleContext().setAccessibleName("\"<\"+\"html\"+\">Crear O Modificar<\"+\"br\"+\">Mapa+\"<!--\"+\"html\"+\"-->\""); // NOI18N
@@ -95,6 +108,10 @@ public class paginaInicial extends javax.swing.JFrame {
         mapaFrame.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -134,6 +151,7 @@ public class paginaInicial extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private vista.PanelFondoPaginaInicial panelFondoPaginaInicial1;
     // End of variables declaration//GEN-END:variables
 }
